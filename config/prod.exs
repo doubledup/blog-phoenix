d51c6c62010cf95ca2ab7063d425fe87ad598fc4
David Dunn <26876072+doubledup@users.noreply.github.com>
2018-11-25 13:57:17 +0200

    Rename db config

--- config/prod.exs
+++ config/prod.exs
@@ -69,7 +69,7 @@ config :logger, level: :info
 # See the releases documentation accordingly.
 
 # Configure the database
-config :hello, Hello.Repo,
+config :teacher, Teacher.Repo,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
